3c30852c045d8f8c1c65674fde4b3abf92078117
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-03-02 16:31:35 +0600

    basic file folder nad func create

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 //parsers
@@ -12,8 +15,16 @@ app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 // );
 // Add this before your routes
 
+// Add this before your routes
+// app.use(fixShurjoPayUrl);
+//application routes
+app.use('/api', router);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello basaFinder !');
 });
+
+app.use(globalErrorHandler);
+app.use(notFound as any);
 
 export default app;
